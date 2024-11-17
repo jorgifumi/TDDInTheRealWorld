57360ce7058a9fd4d34c1ddc88102c04b61a659e
Jorge Lucena <jorgifumi@gmail.com>
2024-11-17 18:13:41 +0100

    Initial Commit

---
+++ TDD/TDD.h
@@ -0,0 +1,18 @@
+//
+//  TDD.h
+//  TDD
+//
+//  Created by Jorge Miguel Lucena Pino on 17/11/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TDD.
+FOUNDATION_EXPORT double TDDVersionNumber;
+
+//! Project version string for TDD.
+FOUNDATION_EXPORT const unsigned char TDDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDD/PublicHeader.h>
+
+
---
+++ TDDTests/TDDTests.swift
@@ -0,0 +1,17 @@
+//
+//  TDDTests.swift
+//  TDDTests
+//
+//  Created by Jorge Miguel Lucena Pino on 17/11/24.
+//
+
+import Testing
+@testable import TDD
+
+struct TDDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
