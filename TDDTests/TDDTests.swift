4c7ec2023622f988f7c45845c0b1b0427a2be41f
Jorge Lucena <jorgifumi@gmail.com>
2024-11-17 19:13:34 +0100

    Make test pass

--- TDDTests/TDDTests.swift
+++ TDDTests/TDDTests.swift
@@ -14,18 +14,18 @@ struct M3U8Parser {
     }
     
     func parse(_ data: Data) throws {
-        
+        throw Error.invalidData
     }
 }
 
 struct TDDTests {
 
     @Test func givenInvalidData_whenParse_ThenThrowError() async throws {
-        let parser = M3U8Parser()
+        let sut = M3U8Parser()
         let invalidData: Data = "invalidData".data(using: .utf8)!
         
         #expect(throws: M3U8Parser.Error.invalidData) {
-            try parser.parse(invalidData)
+            try sut.parse(invalidData)
         }
     }
 }
