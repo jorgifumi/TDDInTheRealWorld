3899911c16e04be3e2dfe2d445416087cd7b130b
Jorge Lucena <jorgifumi@gmail.com>
2024-11-17 19:12:18 +0100

    Write first test that fails

--- TDDTests/TDDTests.swift
+++ TDDTests/TDDTests.swift
@@ -8,10 +8,24 @@
 import Testing
 @testable import TDD
 
+struct M3U8Parser {
+    enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    func parse(_ data: Data) throws {
+        
+    }
+}
+
 struct TDDTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test func givenInvalidData_whenParse_ThenThrowError() async throws {
+        let parser = M3U8Parser()
+        let invalidData: Data = "invalidData".data(using: .utf8)!
+        
+        #expect(throws: M3U8Parser.Error.invalidData) {
+            try parser.parse(invalidData)
+        }
     }
-
 }
